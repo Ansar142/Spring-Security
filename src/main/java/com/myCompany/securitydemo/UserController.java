a8a9056d2b5cdc12aed5ca1d7f6055dce91ef2e5
Ansar142 <130049687+Ansar142@users.noreply.github.com>
2025-08-30 23:38:12 +0500

    Update UserController.java

--- src/main/java/com/myCompany/securitydemo/UserController.java
+++ src/main/java/com/myCompany/securitydemo/UserController.java
@@ -1,6 +1,10 @@
-package com.myCompany.securitydemo;
+package com.myCompany.securitydemo.controller;
 
+import com.myCompany.securitydemo.Service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,23 @@ public class UserController {
 
     @Autowired
     private UserService service;
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @PostMapping("login")
+    public String login(@RequestBody User user){
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken(user.getUserName());
+        else
+            return "Login Failed";
+
+    }
 
     @PostMapping("register")
     public User register(@RequestBody User user) {
